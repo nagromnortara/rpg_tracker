a82fe0dd0f7ab6623460f6f5f79d559766c09675
Antti Vikman <antti.vikman@nolwenture.com>
2026-06-06 12:54:10 +0300

    fix: set data-theme on html element so body background resolves correctly

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/themes.css'
 import App from './App'
 
+document.documentElement.setAttribute('data-theme', 'fallout1945')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename="/rpg_tracker">
